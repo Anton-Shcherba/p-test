92d7546d118c0bb7450266245714705c8596920a
Антон Щерба <Tony.Shcherba@gmail.com>
2022-12-06 11:41:42 +0300

    feat: crate express app

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+let number = 0;
+
+app.get('/', (request, response) => {
+  response.send(`Current num: ${number}`);
+});
+
+app.listen(port, () => console.log(`Running on port ${port}`));
+
+setInterval(function() {
+  number++;
+}, 1000);
